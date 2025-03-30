775f5c7e06cdc8ea96a3a70ab32f9e6e50c62855
Oscar Levin <oscar.levin@gmail.com>
2025-03-30 15:41:46 -0600

    update devcontainer

---
+++ .github/scripts/add-sage.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+
+bash Miniforge3.sh -b -p "/conda"
+
+conda --version
+
+mamba create --yes -n sage sage python=3.12
+
+conda config --set auto_activate_base false
+
+conda init
+
+echo 'conda actiavete sage' >> ~/.bashrc
+
+conda activate sage
+
+pip install pretext[homepage,prefigure] --only-binary {greenlet}
+
+pip install -r requirements.txt
